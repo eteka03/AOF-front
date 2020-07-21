f395b3d274c0fd5c6cbfab226514159d2641bb31
eteka03 <eteka03.akpaki@gmail.com>
2020-07-20 23:25:12 -0400

    work on restaurant presentation part 1

---
+++ components/Gallery/Gallery.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ImageGallery from 'react-image-gallery'
+export default function Gallery({photos}) {
+
+    const gallerie = photos.map(photo => {
+        if(photo.formats.thumbnail){
+            return {original:photo.url ,
+                 thumbnail: photo.formats.thumbnail.url ,
+                 originalAlt: photo.name,
+                 thumbnailALt: photo.name,
+                 originalTitle:photo.name,
+                 thumbnailTitle:photo.name
+                }
+        }else{
+            return {original: photo.url , 
+                thumbnail: photo.url ,
+                originalAlt: photo.name,
+                thumbnailALt: photo.name,
+                originalTitle:photo.name,
+                thumbnailTitle:photo.name
+            }
+        }
+    })
+    return (
+        <ImageGallery lazyLoad={true} items={gallerie} />
+    )
+}
--- pages/restaurants/[id].js
+++ pages/restaurants/[id].js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {useRouter} from 'next/router'
 import axios from 'axios'
-
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAddressBook,faMap, faPhoneAlt, faLink, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {faFacebook, faInstagram, faWhatsapp, faMailchimp} from '@fortawesome/free-brands-svg-icons'
+import Gallery from '../../components/Gallery/Gallery'
 export default function Resto({data,error}) {
 
 const router = useRouter()
@@ -9,7 +13,63 @@ const router = useRouter()
     if(router.isFallback){
         return <div>loading...</div>
     }else{
-        return <div>{data.nom}</div>
+        return (
+
+            <div className="page">
+               <div className="side-presentation">
+                   <div className="data-thumbnail">
+                    <LazyLoadImage src={data.photos[0].formats.small?data.photos[0].formats.small.url : data.photos[1].formats.small.url } width="100%" height="100%" />
+                   </div>
+                   <div className="data-name">
+                     <span>{data.nom}</span>
+                   </div>
+                   <div className="data-menu">
+                        <ul className="list-menu">
+                            <li>Infos</li>
+                            <li>Photos</li>
+                            <li>Avis</li>
+                        </ul>
+                   </div>
+               </div>
+
+
+                <div className="data-main">
+                    <div className="data-infos">
+                            <h4 className="data-title">Informations</h4>
+                            <ul>
+        <li >{data.description || data.nom}</li>
+        <li><FontAwesomeIcon style={{marginRight:'.5rem'}} icon={faMap}/> {data.adresse || 'Burkina Faso'}</li>
+        <li><FontAwesomeIcon style={{marginRight:'.5rem'}} icon={faPhoneAlt}/> {data.telephone || 'Burkina Faso'}</li>
+        <li><FontAwesomeIcon style={{marginRight:'.5rem'}} icon={faEnvelope}/> {data.email || 'Aucun'}</li>
+        <li><FontAwesomeIcon style={{marginRight:'.5rem'}} icon={faLink}/> {data.lien || 'Aucun'}</li>
+        
+        <li><div className="data-social">
+                <FontAwesomeIcon style={{marginRight:'2rem'}} icon={faFacebook} size="2x" />
+                <FontAwesomeIcon style={{marginRight:'2rem'}} icon={faInstagram} size="2x" />
+                <FontAwesomeIcon style={{marginRight:'2rem'}} icon={faWhatsapp} size="2x" />
+            </div></li>
+                            </ul>
+                    </div>
+
+                    <div className="data-photos">
+                        <h4 className="data-title">Photos</h4>
+
+                       <div className="data-gallerie">
+                            <Gallery photos={data.photos} />
+                        </div> 
+                    </div>
+
+                    <div className="data-avis">
+                        <div>les avis</div>
+                    </div>
+                </div>
+                
+
+        <style jsx>{`
+           
+        `}</style>
+            </div>
+        )
     }
     
 
