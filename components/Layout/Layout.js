9336384e7de8947cbb5ff93f127ebb08428bf1e8
eteka03 <eteka03.akpaki@gmail.com>
2020-08-08 00:16:45 -0400

    add favicon

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -16,6 +16,16 @@ export default function Layout({
             <meta charSet="utf-8" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png"/>
+<link rel="manifest" href="/images/icons/site.webmanifest"/>
+<link rel="mask-icon" href="/images/icons/safari-pinned-tab.svg" color="#5bbad5"/>
+<link rel="shortcut icon" href="/images/icons/favicon.ico"/>
+<meta name="msapplication-TileColor" content="#da532c"></meta>
+<meta name="msapplication-config" content="/images/icons/browserconfig.xml"></meta>
+<meta name="theme-color" content="#ffffff"></meta>
         </Head>
 
         <header>
